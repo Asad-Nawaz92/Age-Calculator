a3e37bedd6ff5672c5735dd9b704b8aba2937035
Asad-Nawaz92 <asadn7862@gmail.com>
2023-09-02 21:17:03 +0500

    Age Calculator

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ function calculateAge() {
 
         var age = Math.floor(difference / (1000 * 60 * 60 * 24 * 365))
 
+        document.getElementById('result').style.color = 'white'
+        document.getElementById('result').style.textAlign = 'center'
+        document.getElementById('result').style.fontSize = '20px'
+        document.getElementById('result').style.margin = '50px 450px auto 450px'
+        document.getElementById('result').style.border = '2px solid yellowgreen'
+        document.getElementById('result').style.borderRadius = '25px'
+
         document.getElementById('result').innerHTML = 'Your Age is : ' + age + ' Years'
     }
     else{
